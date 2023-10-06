c17cbb1a01c637b8eb445fd96dbaa63684bb7762
chsavvas11 <ch.savvas28@hotmail.com>
2023-10-06 15:34:02 +0100

    Util to get initials from string (for user icon)

--- client/src/shared/utils.ts
+++ client/src/shared/utils.ts
@@ -1,7 +1,16 @@
 /**
  * The React Fetch method with prefixed API source
- * @param input 
- * @param init 
- * @returns 
+ * @param input
+ * @param init
+ * @returns
  */
-export const apiFetch = (input: RequestInfo | URL, init?: RequestInit | undefined) => fetch(`http://localhost:8000/api/${input}`, init);
+export const apiFetch = (
+    input: RequestInfo | URL,
+    init?: RequestInit | undefined
+) => fetch(`http://localhost:8000/api/${input}`, init);
+
+export function getInitials(inputString: string): string {
+    const words = inputString.split(" ");
+    const initials = words.map((word) => word.charAt(0).toUpperCase());
+    return initials.join("");
+}
