5d977d4ddfadf6f4ae50ac0422efca025d891fb1
chsavvas11 <ch.savvas28@hotmail.com>
2023-10-06 15:38:47 +0100

    Quests, invest, goals page/route setup

---
+++ client/src/app/(wrappers)/(authenticated)/(navigation)/invest/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="h-full w-full text-gray-900 flex items-center justify-center text-5xl">
+            INVEST
+        </div>
+    );
+}
