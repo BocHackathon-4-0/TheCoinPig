bac0ed8b33a284058c5041bece69fe2c916b595c
NikolasTh90 <41706535+NikolasTh90@users.noreply.github.com>
2023-10-07 02:51:40 +0300

    goal model associate with user

--- server/Goals/models.py
+++ server/Goals/models.py
@@ -4,8 +4,11 @@ from django.db import models
 class Goals(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField()
-    target_balance = models.FloatField()
+    target_balance = models.FloatField(default=0)
     achieved = models.BooleanField(default=False)
+    reward = models.FloatField(default=0)
+
+    user = models.ForeignKey('MyUsers.ChildUser', on_delete=models.CASCADE, related_name='child_goals', null=True)
     
 
     def __str__(self):
