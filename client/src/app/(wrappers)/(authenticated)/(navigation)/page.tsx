b423493a942381b3ff0c0ee75c4e32fc59ca166d
chsavvas11 <ch.savvas28@hotmail.com>
2023-10-06 16:32:51 +0100

    Overview balance component

--- client/src/app/(wrappers)/(authenticated)/(navigation)/page.tsx
+++ client/src/app/(wrappers)/(authenticated)/(navigation)/page.tsx
@@ -1,7 +1,70 @@
+import { OverviewBalance } from "@/components/overview-balance";
+import { Box, Container, Grid } from "@mui/material";
+
 export default function Home() {
     return (
-        <div className="h-full w-full text-gray-900 flex items-center justify-center text-5xl">
-            HELLO BOC HACKATHON
-        </div>
+        <Box
+            component="main"
+            sx={{
+                flexGrow: 1,
+                py: 8,
+            }}
+        >
+            <Container maxWidth="xl">
+                <Grid container spacing={3}>
+                    <Grid xs={12} sm={6} lg={3}>
+                        <OverviewBalance sx={{ height: "100%" }} />
+                    </Grid>
+                    <Grid xs={12} sm={6} lg={3}>
+                        {/* <OverviewTotalCustomers
+                            difference={16}
+                            positive={false}
+                            sx={{ height: "100%" }}
+                            value="1.6k"
+                        /> */}
+                    </Grid>
+                    <Grid xs={12} sm={6} lg={3}>
+                        {/* <OverviewTasksProgress
+                            sx={{ height: "100%" }}
+                            value={75.5}
+                        /> */}
+                    </Grid>
+                    <Grid xs={12} sm={6} lg={3}>
+                        {/* <OverviewTotalProfit
+                            sx={{ height: "100%" }}
+                            value="$15k"
+                        /> */}
+                    </Grid>
+                    <Grid xs={12} lg={8}>
+                        {/* <OverviewSales
+                            chartSeries={[
+                                {
+                                    name: "This year",
+                                    data: [
+                                        18, 16, 5, 8, 3, 14, 14, 16, 17, 19, 18,
+                                        20,
+                                    ],
+                                },
+                                {
+                                    name: "Last year",
+                                    data: [
+                                        12, 11, 4, 6, 2, 9, 9, 10, 11, 12, 13,
+                                        13,
+                                    ],
+                                },
+                            ]}
+                            sx={{ height: "100%" }}
+                        /> */}
+                    </Grid>
+                    <Grid xs={12} md={6} lg={4}>
+                        {/* <OverviewTraffic
+                            chartSeries={[63, 15, 22]}
+                            labels={["Desktop", "Tablet", "Phone"]}
+                            sx={{ height: "100%" }}
+                        /> */}
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
     );
 }
