be4893273d4b3bd82722d3bdda19ceb9a35c3d56
rafael <rafailmaximos@hotmail.com>
2023-10-07 18:54:25 +0300

    added Serializers to models|fetching quests, quizes, etc

--- server/Quests/urls.py
+++ server/Quests/urls.py
@@ -2,8 +2,21 @@ from django.contrib import admin
 from django.urls import path
 from Quests.views import (
     GetQuests,
+    GetQuestsByCategory,
+    GetCategories,
+    GetArticles,
+    GetQuestQuiz,
+    GetQuestRelatedArticle,
+    
 )
 
 urlpatterns = [
     path('get_quests/', GetQuests.as_view()),
+    path('get_quests_categories/', GetCategories.as_view()),
+    path('get_quests_article/', GetArticles.as_view()),
+    path('get_quest_quiz/', GetQuestQuiz.as_view()),
+    path('get_quests_by_category/', GetQuestsByCategory.as_view()),
+    path('get_quest_article/', GetQuestRelatedArticle.as_view()),
+
+
 ]
