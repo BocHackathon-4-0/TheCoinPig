d6dc20c48bdd4477e1b38fad5494a9ef07a8230d
NikolasTh90 <41706535+NikolasTh90@users.noreply.github.com>
2023-10-07 20:01:32 +0300

    stringify type if user

--- server/MyUsers/views.py
+++ server/MyUsers/views.py
@@ -26,7 +26,7 @@ class CheckUserCredentials(APIView):
                 "message": "User exists",
                 "user_id": user_id,
                 "balance": balance,
-                "permission": type(curr_user)
+                "permission": str(type(curr_user))
             }
 
             status_code = status.HTTP_200_OK
