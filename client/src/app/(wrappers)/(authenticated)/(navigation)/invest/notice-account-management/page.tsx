3d027a2eeb062c018d9f45b1863bdb3cbf4c0992
xxristoss <chrischristodoulou06@gmail.com>
2023-10-07 16:42:30 +0100

    Added notice account creation and management page.

---
+++ client/src/app/(wrappers)/(authenticated)/(navigation)/invest/notice-account-management/page.tsx
@@ -0,0 +1,15 @@
+import { Checkbox } from "@mui/material"
+import EuroIcon from '@mui/icons-material/Euro';
+import Link from 'next/link';
+
+export default function Page() {
+    return (
+        <div className="relative w-full h-full overflow-y-auto pb-16">
+            <div className="relative z-0 flex flex-col items-center justify-center w-full px-2 bg-white xxs:px-4 py-14 sm:py-12 lg:py-16 isolate lg:px-8">
+                <div className="z-20 max-w-2xl mx-auto text-center">
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Notice Account Management</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
