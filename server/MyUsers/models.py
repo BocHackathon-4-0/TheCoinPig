8970e4bd6287f1372c4f8d37676ef61e0b6af3c2
NikolasTh90 <41706535+NikolasTh90@users.noreply.github.com>
2023-10-07 20:10:40 +0300

    type fix

--- server/MyUsers/models.py
+++ server/MyUsers/models.py
@@ -5,18 +5,18 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     name = models.CharField(max_length=255)
     balance = models.FloatField(default=0)
+    is_parent = models.BooleanField(default=True)
+
 
     def __str__ (self):
         return self.name
     
 
 class ParentUser(User):
-    is_parent = models.BooleanField(default=True)
     class Meta:
         verbose_name_plural = 'Parent Users'
 
 class ChildUser(User):
-    is_parent = models.BooleanField(default=False)
     parent = models.ForeignKey(ParentUser, on_delete=models.CASCADE, related_name='children')
     completed_quests = models.ManyToManyField('Quests.Quest', blank=True, related_name='completed_quests')
     
