e1dc2f8b29b9d2e75ee83f84c1e2619093da0be4
xxristoss <chrischristodoulou06@gmail.com>
2023-10-07 21:34:18 +0100

    Navbar colour edit.

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -51,7 +51,7 @@ export default function NavBar() {
     };
 
     return (
-        <AppBar position="static">
+        <AppBar position="static" sx={{ backgroundColor: '#ec4899' }}>
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
                     <LogoIcon
