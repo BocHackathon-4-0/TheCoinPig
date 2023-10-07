2dc34d4bb095796fd66f9e970539be848c7ef244
xxristoss <chrischristodoulou06@gmail.com>
2023-10-07 20:49:35 +0100

    Added notice account card in invest page.

--- client/src/app/(wrappers)/(authenticated)/(navigation)/invest/page.tsx
+++ client/src/app/(wrappers)/(authenticated)/(navigation)/invest/page.tsx
@@ -5,6 +5,8 @@ import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 import TrendingDownIcon from "@mui/icons-material/TrendingDown";
 import BuyProductDialog from "@/components/buy-product-dialog";
+import Image from "next/image";
+import BOCIcon from '../../../../../../public/partners-logo/Bank_of_Cyprus-Logo.png';
 
 type TPortfolio = {
     name: string;
@@ -108,7 +110,7 @@ export default function Page() {
     return (
         <div className="overflow-y-auto h-full">
             <div className="flex-col items-center mx-auto justify-center max-w-7xl shadow-sm">
-                <h1 className="text-3xl font-extrabold text-gray-900 px-4 pt-16">
+                <h1 className="text-4xl font-extrabold text-gray-900 px-4 pt-16 pb-2 text-center">
                     My Portfolio
                 </h1>
                 <div className="flex items-center">
@@ -121,7 +123,7 @@ export default function Page() {
                                             <div className="text-sm text-gray-500">
                                                 <div className="">
                                                     <a
-                                                        href="#"
+                                                        href=""
                                                         className="inline-block rounded-full text-white bg-blue-700 text-xs font-bold mr-1 md:mr-2 mb-2 px-2 md:px-4 py-1"
                                                     >
                                                         {investment.name}
@@ -145,11 +147,103 @@ export default function Page() {
                 </div>
                 <div className="bg-white pt-8 pb-24">
                     <div className="mx-auto w-7xl px-4">
-                        <div className=" max-w-2xl">
-                            <h2 className="mx-auto text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                                Purchase Options
-                            </h2>
-                        </div>
+                        <h2 className="mx-auto text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
+                            Investment Options
+                        </h2>
+                        <div className="mt-16 rounded-xl bg-gray-200 sm:mt-10 lg:mx-0 lg:flex lg:w-7xl">
+                                <div className="p-8 sm:p-10 lg:flex-auto">
+                                    <div className="flex items-center">
+                                        <h3 className="text-4xl font-bold tracking-tight text-gray-900 mr-2">
+                                            Notice Account | 
+                                        </h3>
+                                        <Image
+                                            src={BOCIcon}
+                                            alt="Bank of Cyprus logo"
+                                            className="w-[240px] h-auto"
+                                        />
+                                    </div>
+                                    <p className="mt-6 text-lg leading-7 text-gray-600">
+                                        Notice accounts require advance notice for withdrawals, balancing accessibility with potentially higher interest rates.
+                                    </p>
+                                    <div className="mt-6 flex items-center gap-x-4">
+                                        <h4 className="flex-none text-lg font-semibold leading-6 text-blue-600">
+                                            Return frequency: 3 months
+                                        </h4>
+                                    </div>
+                                    <ul
+                                        role="list"
+                                        className="mt-8 grid grid-cols-1 gap-4 text-xl leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
+                                    >
+                                        <li className="flex gap-x-3">
+                                            <svg
+                                                className="h-6 w-5 flex-none text-blue-600"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                                aria-hidden="true"
+                                            >
+                                                <ThumbUpIcon />
+                                            </svg>
+                                            Success rate: 100%
+                                        </li>
+                                        <li className="flex gap-x-3">
+                                            <svg
+                                                className="h-6 w-5 flex-none text-blue-600"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                                aria-hidden="true"
+                                            >
+                                                <TrendingUpIcon />
+                                            </svg>
+                                            Profit yield: 0.2-1.0%
+                                        </li>
+                                        <li className="flex gap-x-3">
+                                            <svg
+                                                className="h-6 w-5 flex-none text-blue-600"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                                aria-hidden="true"
+                                            >
+                                                <ThumbDownIcon />
+                                            </svg>
+                                            Fail rate: 0%
+                                        </li>
+                                        <li className="flex gap-x-3">
+                                            <svg
+                                                className="h-6 w-5 flex-none text-blue-600"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                                aria-hidden="true"
+                                            >
+                                                <TrendingDownIcon />
+                                            </svg>
+                                            Loss yield: 0%
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+                                    <div className="rounded-2xl bg-gradient-to-br from-white via-blue-100 to-white py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+                                        <div className="mx-auto max-w-xs px-8">
+                                            <p className="text-base font-semibold text-gray-600">
+                                                Minimum investment
+                                            </p>
+                                            <p className="mt-6 flex items-baseline justify-center gap-x-2">
+                                                <span className="text-5xl font-bold tracking-tight text-gray-900">
+                                                    10
+                                                </span>
+                                                <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">
+                                                    Euro
+                                                </span>
+                                            </p>
+                                            <a href="notice-account-creation" className="mt-10 block w-full rounded-md bg-blue-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
+                                                Open account
+                                            </a>
+                                            <p className="mt-6 text-xs leading-5 text-gray-600">
+                                                T & Cs apply.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         {investmentProducts.map((product) => (
                             <div className="mt-16 rounded-xl bg-gray-200 sm:mt-10 lg:mx-0 lg:flex lg:w-7xl">
                                 <div className="p-8 sm:p-10 lg:flex-auto">
@@ -211,7 +305,7 @@ export default function Page() {
                                             >
                                                 <TrendingDownIcon />
                                             </svg>
-                                            Loss yield{product.lossYield}
+                                            Loss yield: {product.lossYield}
                                         </li>
                                     </ul>
                                 </div>
