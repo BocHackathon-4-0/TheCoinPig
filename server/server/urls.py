e3126d37a20557dcf601e25db75e7f0b989c3eb5
NikolasTh90 <41706535+NikolasTh90@users.noreply.github.com>
2023-10-08 03:14:42 +0300

    serve media

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,9 @@ urlpatterns = [
     path('goals/', include('Goals.urls')),
     path('investments/', include('Investments.urls')),
 ]
+
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
